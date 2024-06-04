e7cfbb3fe4081b4d531ca10c50b9e46ecac37365
Cyberfrosch <129263807+Cyberfrosch@users.noreply.github.com>
2024-06-05 04:42:14 +0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -84,7 +84,7 @@ int main( int argc, char* argv[] )
 {
     try
     {
-        if ( argc != 2 )
+        if (argc != 2 )
         {
             std::cerr << "Usage: chat_server <port>" << std::endl;
             return 1;
